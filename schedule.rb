159ef3f683640d6b869100da8ae2c608814ce167
bellvat <msirathanarun@gmail.com>
2018-06-17 17:10:01 -0700

    Adding Start class and appointment module

--- schedule.rb
+++ schedule.rb
@@ -9,31 +9,64 @@
 #When i make an appointment, it should update both the patient, and the doctor objects
 #before appointment made, need to check if doctor is available
 
+module Appointment
+  def add_appointment(date, person)
+    @appointments[date] ||= [] 
+    @appointments[date] << person.name
+  end
+end
+
 class Patient
-  def init(name)
+  attr_reader :name
+  include Appointment
+  def initialize(name)
     @name = name
-    @appointments = []
+    @appointments = {}
+  end
+
+  def make_appt(date, doctor, patient)
+    if doctor.available?(date)
+      doctor.add_appointment(date,patient)
+      patient.add_appointment(date,doctor)
+      puts "Congrats, appointment booked"
+    else
+      puts "Sorry, Doctor is not available"
+    end
   end
 end
 
 class Doctor
-  def init(name)
+  attr_reader :appointments, :name
+  include Appointment
+  def initialize(name)
     @name = name
     #appointment key = date, value = patient
     @appointments = {}
   end
 
-  def add_appointment(date, patient)
-    @appointments[date] = patient  
-  end
   #instance method
-  def available? 
-    appointment
+  def available?(date)
+    val = @appointments[date]
+    
+    return true if @appointments.empty? || val.nil? || val.length < 2
+    return false
   end
 end
 
-class Appointment
-  def check_available(doctor)
-    doctor.check_available_method 
-  end
+
+class Start
+  steve = Doctor.new('steve')
+  barb = Patient.new('barb')
+  carrie = Patient.new('carrie')
+  liz = Patient.new('liz')
+  james = Patient.new('james')
+  barb.make_appt('5/12/18',steve,barb)
+  carrie.make_appt('5/12/18',steve,carrie)
+  puts steve.appointments
+  liz.make_appt('5/13/18',steve,liz)
+  puts steve.appointments
+  james.make_appt('5/12/18',steve,james)
+  puts steve.appointments
+
 end
+
